c5537e89cb9f66ac95fada92cea496d14bbbbc57
ds <devin8086@outlook.com>
2025-02-09 15:31:59 -0800

    Day 1 of trying to make Windows 11 usable

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Windows11
+# Windows 11
+You thought Vista was bad?! Buckle up buttercup, you've never been *less* in control of what your operating system is doing than with Windows 11.
+Back in the day, we all turned to [Black Viper](https://www.blackviper.com/) for Windows tuning tips, this repo is an homage to that lost art.
+
+# Mission
+Settings will be configured via local GPO whenever possible. I feel these changes have the best chance of surviving revert via a patch.
+What cannot be configured via local GPO, will be set via registry. Native PowerShell cmdlets will be used for app removal and the like.
+
+# FAQs
+Q: What if I like Windows 11 the way it is?
+A: This is not the repo you are looking for.
+
+Q: Should I use this in my corporate network?
+A: Probably not, but I'm not your supervisor.
+
---
+++ script.ps1
@@ -0,0 +1,65 @@
+﻿# Applications I never asked for
+Function Applications {
+
+# Applications to remove via Winget
+$WingetRemove = @(
+    'OneDrive',
+    'Microsoft.Teams',
+    'Copilot',
+    'Quick Assist',
+    'Dev Home',
+    'Microsoft To Do',
+    'Power Automate',
+    'Microsoft Bing Search',
+    'Microsoft News',
+    'Outlook for Windows'
+)
+
+# Winget source agreements
+winget list --accept-source-agreements | Out-Null
+
+# Remove applications in array above
+$i = 0
+foreach ($wr in $WingetRemove) {
+    $i++
+    Write-Progress "Removing bloatware: $wr" -PercentComplete ($i / $WingetRemove.Count * 100)
+    winget uninstall $wr
+}
+
+}
+
+Function Registry {
+
+# Taskbar alignment left... it's always been left
+$taskbarAl = 0000000
+New-ItemProperty `
+    -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
+    -Name "TaskbarAl" `
+    -Value "0" `
+    -PropertyType "DWORD" `
+    -Force
+
+# Disable transparency
+New-ItemProperty `
+    -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" `
+    -Name "EnableTransparency" `
+    -Value "0" `
+    -PropertyType "DWORD" `
+    -Force
+
+# Disable animations
+New-ItemProperty `
+    -Path "HKCU:\Control Panel\Desktop" `
+    -Name "AnimationDuration" `
+    -Value "0" `
+    -PropertyType "DWORD" `
+    -Force
+}
+
+Function GroupPolicy {
+    .\LGPO.exe /g .\gpo
+}
+
+Applications
+Registry
+GroupPolicy
